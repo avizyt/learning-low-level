11940e96827b04b2381cd40f67e3db576f60ad28
Avijit Biswas <avijit.biswas27@outlook.com>
2025-04-27 15:01:37 +0530

    anagram checker

--- grokking-pattern-problems/warmup/anagram.cpp
+++ grokking-pattern-problems/warmup/anagram.cpp
@@ -1,15 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool IsAnagram(string word1, string word2)
+bool IsAnagram(const string &word1, const string &word2)
 {
-    unordered_map<char, int> m1;
-    unordered_set<char> s2;
-
-    for (char ch : word1)
+    if (word1.length() != word2.length())
     {
-        if ()
+        return false;
     }
+
+    vector<int> count(26, 0); // 26 letters
+
+    for (int i = 0; i < word1.length(); ++i)
+    {
+        count[word1[i] - 'a']++;
+        count[word2[i] - 'a']--;
+    }
+
+    for (int val : count)
+    {
+        if (val != 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main()
@@ -17,6 +31,16 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
+    string s = "anagram";
+    string t = "nagaram";
+
+    cout << boolalpha << IsAnagram(s, t) << endl;
+
+    string x = "rat";
+    string y = "car";
+
+    cout << boolalpha << IsAnagram(x, y) << endl;
 
     return 0;
 }
