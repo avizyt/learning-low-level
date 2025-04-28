11cbe0cfa20f8ccc06d77884cedb99f4dd6aa0b8
Avijit Biswas <avijit.biswas27@outlook.com>
2025-04-28 10:57:57 +0530

    shortest wrd distance

---
+++ foundation-l1/basic-cpp/type-deduction.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void f(ParamType param);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    f(expr);
+
+    return 0;
+}
---
+++ grokking-pattern-problems/warmup/word-shortest-dist.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ShortestDistance(vector<string> &words, const string &word1, const string &word2)
+{
+    int index1 = -1, index2 = -1;
+    int minDistance = INT_MAX;
+
+    for (int i = 0; i < words.size(); ++i)
+    {
+        if (words[i] == word1)
+        {
+            index1 = i;
+        }
+        else if (words[i] == word2)
+        {
+            index2 = i;
+        }
+
+        // olny calculate distance if both words have fund at least once
+        if (index1 != -1 && index2 != -1)
+        {
+            minDistance = min(minDistance, abs(index1 - index2));
+        }
+    }
+    return minDistance;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // vector<string> words = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
+    vector<string> words = {"a", "c", "d", "b", "a"};
+    // string word1 = "fox";
+    string word1 = "a";
+    // string word2 = "dog";
+    string word2 = "b";
+
+    cout << ShortestDistance(words, word1, word2) << endl;
+
+    return 0;
+}
